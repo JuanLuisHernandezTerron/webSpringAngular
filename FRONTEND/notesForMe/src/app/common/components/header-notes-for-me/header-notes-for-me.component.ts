bc0dc101dab641d6e4c8f83648085e676c90c4d8
unknown <you@example.com>
2023-12-26 13:36:07 +0100

    mejoras impl

--- FRONTEND/notesForMe/src/app/common/components/header-notes-for-me/header-notes-for-me.component.ts
+++ FRONTEND/notesForMe/src/app/common/components/header-notes-for-me/header-notes-for-me.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header-notes-for-me',
   templateUrl: './header-notes-for-me.component.html',
   styleUrls: ['./header-notes-for-me.component.scss']
 })
-export class HeaderNotesForMeComponent {
+export class HeaderNotesForMeComponent implements OnInit{
+
+  public isLogged:Boolean = false;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
 
 }
